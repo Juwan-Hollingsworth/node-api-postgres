3a9492b6e42531e57e109b72c4b235facf59467e
Juwan <jhollingsworth5@student.clayton.edu>
2023-06-23 20:57:04 -0400

    get all users from db & send results via response to client

--- queries.js
+++ queries.js
@@ -1,3 +1,18 @@
 const Pool = require("pg").Pool;
 const config = require("./config");
 const pool = new Pool({ config });
+
+// creating endpoints for CRUD operations
+
+//Get all users f(x)
+const getUsers = (request, response) => {
+  //execute SQL query against db
+
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    //execute once db transaction complete
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
