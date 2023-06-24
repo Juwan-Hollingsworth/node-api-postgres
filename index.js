47ac02fecf20786a5593b0f042555ef93946181c
Juwan <jhollingsworth5@student.clayton.edu>
2023-06-23 20:36:39 -0400

    add server setup w middleware - init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// import modules & middleware
+const express = require("express");
+const bodyParser = require("body-parser");
+//create instance of express app
+const app = express();
+//define var for port # which app will listen 4 requests
+const port = 3000;
+
+//parse json encoded data sent thru request body
+app.use(bodyParser.json());
+//parse url encoded data also
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+//set app to listen to port
+app.listen(port, () => {
+  console.log(`App is running on port ${port} 🙂`);
+});
